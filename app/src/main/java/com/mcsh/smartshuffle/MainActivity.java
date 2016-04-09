87e5d717fd3897239f36ea3cb5a05e7555a4cb09
MCSH <MCSHEMAIL@gmail.com>
2016-04-09 13:17:40 +0430

    added base variables

--- app/src/main/java/com/mcsh/smartshuffle/MainActivity.java
+++ app/src/main/java/com/mcsh/smartshuffle/MainActivity.java
@@ -9,7 +9,18 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import android.net.Uri;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.widget.ListView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Song> songList;
+    private ListView songView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
